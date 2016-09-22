527a307db20d5a0ec2fb40f45fa4f9b8b3dabe5b
Pascal MARTIN <pmartin@tea-ebook.com>
2016-09-22 12:40:00 +0200

    rename app + url in defines

--- defines.h
+++ defines.h
@@ -2,13 +2,13 @@
 #define DEFINES_H_
 
 
-#define PLOP_APPLICATION_FULLNAME "Belladonna Reader"
-#define PLOP_APPLICATION_SHORTNAME "Belladonna"
+#define PLOP_APPLICATION_FULLNAME "Plop! reader"
+#define PLOP_APPLICATION_SHORTNAME "Plop!R"
 
 #define PLOP_VERSION_STR "v0.1.0"
 #define PLOP_VERSION_NUM 1
-#define PLOP_WEBSITE_URL "http://belladonna-reader.org"
-#define PLOP_OPENSOURCE_URL "https://github.com/pmartin/belladonna"
+#define PLOP_WEBSITE_URL "http://plop-reader.pascal-martin.fr"
+#define PLOP_OPENSOURCE_URL "https://github.com/pmartin/plop-reader"
 
 // On a TL3: /mnt/ext1/system/usr/share/plop-reader
 #define PLOP_BASE_DIRECTORY USERDATA "/share/plop-reader"

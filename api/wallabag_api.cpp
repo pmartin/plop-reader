e01a5ed61a0ddded5d108866c3c0aec63dadeaad
Pascal MARTIN <pmartin@tea-ebook.com>
2016-09-27 12:37:38 +0200

    An error while syncing to server now aborts sync, with a clear error message

--- api/wallabag_api.cpp
+++ api/wallabag_api.cpp
@@ -353,7 +353,13 @@ void WallabagApi::syncOneEntryToServer(EntryRepository repository, Entry &entry)
 	};
 
 	auto onFailure = [this] (CURLcode res, long response_code, CURL *curl) -> void {
-		// TODO error-handling
+		ERROR("API: syncOneEntryToServer(): failure. HTTP response code = %ld", response_code);
+
+		std::ostringstream ss;
+		ss << "Could not sync entry to server: server returned a ";
+		ss << response_code;
+		ss << " status code.";
+		throw SyncOAuthException(ss.str());
 	};
 
 	DEBUG("API: syncOneEntryToServer(%d)", entry.id);

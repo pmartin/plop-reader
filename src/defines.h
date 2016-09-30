6ccb6d82fec630fe24bea570a6fec170bf2ee561
Pascal MARTIN <pmartin@tea-ebook.com>
2016-09-30 13:05:12 +0200

    Version 0.2.0

--- src/defines.h
+++ src/defines.h
@@ -5,8 +5,8 @@
 #define PLOP_APPLICATION_FULLNAME "Plop! reader"
 #define PLOP_APPLICATION_SHORTNAME "Plop!R"
 
-#define PLOP_VERSION_STR "v0.1.0"
-#define PLOP_VERSION_NUM 1
+#define PLOP_VERSION_STR "v0.2.0"
+#define PLOP_VERSION_NUM 2
 #define PLOP_WEBSITE_URL "http://plop-reader.pascal-martin.fr"
 #define PLOP_OPENSOURCE_URL "https://github.com/pmartin/plop-reader"
 

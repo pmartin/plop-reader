599cbadebdba46fe1cc97a886954e9b777c14eaa
Pascal MARTIN <pmartin@tea-ebook.com>
2016-11-03 13:46:19 +0100

    Entity EpubDownload

---
+++ src/entities/epub_download.h
@@ -0,0 +1,19 @@
+#ifndef SRC_ENTITIES_EPUB_DOWNLOAD_H_
+#define SRC_ENTITIES_EPUB_DOWNLOAD_H_
+
+
+class EpubDownload
+{
+public:
+
+	int entry_id;
+
+	int downloading_at;
+	int downloaded_at;
+
+	int created_at;
+
+};
+
+
+#endif /* SRC_ENTITIES_EPUB_DOWNLOAD_H_ */

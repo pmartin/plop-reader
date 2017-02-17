5813c2e6ee50432d44ebd0b74e4d435109f28e9b
Pascal MARTIN <pmartin@tea-ebook.com>
2017-02-17 12:28:53 +0100

    Display icons for item infos

--- src/gui/gui_list_item_entry.cpp
+++ src/gui/gui_list_item_entry.cpp
@@ -41,8 +41,50 @@ void GuiListItemEntry::draw(bool clearBeforeDraw, bool mustUpdateScreen, bool hi
 
 
 	SetFont(infosFont, entry.is_empty ? LGRAY : DGRAY);
-	snprintf(buffer, sizeof(buffer), "#%d/%s a%d/%d *%d/%d u%d/%d t%d %s", entry.id, entry.remote_id.c_str(), entry.local_is_archived, entry.remote_is_archived, entry.local_is_starred, entry.remote_is_starred, entry.local_updated_at, entry.remote_updated_at, (int)ceil((float)entry.reading_time/(float)60), (entry.local_content_file_epub != "" ? "E" : "H"));
-	DrawString(textX, yy, buffer);
+	int xInfos = textX;
+
+	// Date de mise à jour (serveur)
+	DrawBitmap(xInfos, yy, smallEvent);
+	xInfos += smallEvent->width + 2;
+	time_t t = entry.remote_created_at;
+	struct tm *tm = localtime(&t);
+	strftime(buffer, sizeof(buffer), "%Y-%m-%d", tm);
+	DrawString(xInfos, yy-2, buffer);
+	xInfos += StringWidth(buffer);
+	xInfos += 12;
+
+	// Durée de lecture
+	DrawBitmap(xInfos, yy, smallClock);
+	xInfos += smallClock->width + 2;
+	snprintf(buffer, sizeof(buffer), "%dmin", (int)ceil((float)entry.reading_time/(float)60));
+	DrawString(xInfos, yy-2, buffer);
+	xInfos += StringWidth(buffer);
+	xInfos += 12;
+
+	xInfos = screenWidth - 6;
+
+	// Type de fichier disponible
+	if (entry.local_content_file_epub != "") {
+		xInfos -= smallEpub->width + 2;
+		DrawBitmap(xInfos, yy, smallEpub);
+	}
+	else {
+		xInfos -= smallHtml->width + 2;
+		DrawBitmap(xInfos, yy, smallHtml);
+	}
+	xInfos -= 6;
+
+	// Starred ?
+	if (entry.local_is_starred) {
+		xInfos -= smallStarred->width + 2;
+		DrawBitmap(xInfos, yy, smallStarred);
+		xInfos -= 6;
+	}
+
+
+	//snprintf(buffer, sizeof(buffer), "#%d/%s a%d/%d *%d/%d u%d/%d t%d %s", entry.id, entry.remote_id.c_str(), entry.local_is_archived, entry.remote_is_archived, entry.local_is_starred, entry.remote_is_starred, entry.local_updated_at, entry.remote_updated_at, (int)ceil((float)entry.reading_time/(float)60), (entry.local_content_file_epub != "" ? "E" : "H"));
+	//DrawString(textX + 100, yy, buffer);
+
 	yy += infosFont->height;
 
 	SetFont(infosFont, entry.is_empty ? LGRAY : DGRAY);
